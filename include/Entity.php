7dbbc21a326b7258b5c3d077341ffd0800f69592
Mike Whiting <mail@mikejw.co.uk>
2009-05-20 16:41:08 +0000

    bug in entity class.

--- include/Entity.php
+++ include/Entity.php
@@ -15,7 +15,7 @@
   // You should have received a copy of the GNU Lesser General Public License
   // along with Empathy.  If not, see <http://www.gnu.org/licenses/>.
 
-class  EntityNew
+class Entity
 {
   private $val;
   private $controller;
