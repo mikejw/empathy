3c29c7399b46c76dd7f039be1a3b13fffd90eb15
Mike Whiting <mail@mikejw.co.uk>
2009-09-20 20:51:59 +0000

    updating architype.

--- eaa/public_html/index.php
+++ eaa/public_html/index.php
@@ -1,5 +1,5 @@
 <?php
-require('config.php');
+require('config_by_yaml.php');
 include('empathy/include/Bootstrap.php');
-$boot = new Bootstrap($module, $moduleIsDynamic);
+$boot = new Bootstrap($config['module'], $config['module_is_dynamic'], $config['specialised']);
 ?>
