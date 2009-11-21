8add342c10774d83e27a9ba2bc3451364569f1b3
Mike Whiting <mail@mikejw.co.uk>
2009-11-21 20:22:27 +0000

    removing old template file and preparing to start on library unit testing.

---
+++ t/lib/BootstrapTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'PHPUnit/Extensions/OutputTestCase.php';
+
+class BootstrapTest extends PHPUnit_Framework_TestCase
+{
+
+  protected function setUp()
+  {
+
+
+  }
+
+
+  public function testSomething()
+  {
+    $this->assertTrue(true);
+  }
+}
+?>
