9173ed7df367910ef43066d4b04d111f8a9bacec
Mike Whiting <mail@mikejw.co.uk>
2010-08-03 11:48:31 +0000

    adding microtime wrapper function.

--- lib/Util/CLI.php
+++ lib/Util/CLI.php
@@ -8,20 +8,29 @@ namespace Empathy\Util;
 
 class CLI
 {
+  
+  // new function
+  private static function realMicrotime()
+  {
+    list($micro, $seconds) = explode(' ', microtime());
+    return ((float)$micro + (float)$seconds);
+  }
+
   public static function request($e, $uri)
   {
     ob_start();
-    $t_request_start = microtime();    
+    $t_request_start = self::realMicrotime();    
     $_SERVER['REQUEST_URI'] = $uri;
 
     $e->beginDispatch();
-    $t_request_finish = microtime();
+    $t_request_finish = self::realMicrotime();
     $response = ob_get_contents();    
     ob_end_clean();
-    $t_elapsed = ($t_request_finish - $t_request_start);
+    $t_elapsed = ($t_request_finish - $t_request_start);    
     $t_elapsed = number_format($t_elapsed, 4);
-    return $t_elapsed;
+
     //return $response;
+    return $t_elapsed;    
   }
 
 
