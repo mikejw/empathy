108d8c23adf455146af619de80fc4d535c9a0d74
Mike Whiting <mail@mikejw.co.uk>
2011-04-15 17:56:26 +0000

    adding phirehose plugin.

---
+++ plugins/Phirehose-1.0.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Empathy\Plugin;
+use Empathy\Plugin as Plugin;
+
+class Phirehose
+{
+
+  public function __construct()
+  {     	  
+	require('phirehose/Phirehose.php');
+  }
+
+
+}
+?>
