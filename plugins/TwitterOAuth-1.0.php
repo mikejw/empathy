ade06834a39d76bd91a1723f2253867dfd6e1e97
Mike Whiting <mail@mikejw.co.uk>
2011-05-26 19:35:34 +0000

    adding TwitterOAuth plugin class.

---
+++ plugins/TwitterOAuth-1.0.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Empathy\Plugin;
+use Empathy\Plugin as Plugin;
+
+class TwitterOAuth
+{
+
+  public function __construct()
+  {
+    require('twitteroauth/twitteroauth.php');
+  }
+
+  
+
+
+}
+?>
