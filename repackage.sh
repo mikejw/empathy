2b10118ebf4a8ac4c65b4d3d7954df42eb3d264a
Mike Whiting <mail@mikejw.co.uk>
2012-04-06 19:19:00 +0100

    working on base app archive.

--- repackage.sh
+++ repackage.sh
@@ -18,8 +18,13 @@ if [ -e ./get/${PACKAGE_NAME}-${VERSION}.tar ]; then
     pirum build ${CHANNEL_ROOT}
 fi
 
-# repackage and add back to channel
+# return to source dir
 cd ${CWD}
+
+# bundle up base app architype
+zip -r ./Empathy/eaa.zip eaa/
+
+# repackage and add back to channel
 cat ./package.xml.base | sed s/CURRENT_DATE/${DATE}/ | sed s/CURRENT_TIME/${TIME}/ > package.xml
 pear package
 pirum add ${CHANNEL_ROOT} ${PACKAGE_NAME}-${VERSION}.tgz
