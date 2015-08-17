b9434d188fdda6fccae5e28b3332b0f815577762
Mike Whiting <mail@mikejw.co.uk>
2015-08-17 13:35:11 +0200

    playing with psr7 stuff.

--- t/Experimental/MessageTest.php
+++ t/Experimental/MessageTest.php
@@ -7,22 +7,29 @@ use ESuite\ESuiteTest;
 
 class MessageTest extends ESuiteTest
 {
+    private $message;
     
+
     protected function setUp()
     {
-        
+        $this->message = new \ESuite\Fake\Message();    
     }
 
-    
-    public function testInit()
+    public function testGetProtocolVersion()
     {
-	    echo 1;
-	    try {
-        	$m = new \ESuite\Fake\Message();
-		} catch (\Exception $e) {
-			echo $e->getMessage();
-		}    	    
+        $this->assertEquals('1.1', $this->message->getProtocolVersion());
+    }
 
+    public function testWithProtocolVersion()
+    {
+        $version = '1.0';
+        $this->assertInstanceOf('ESuite\Fake\Message', $this->message->withProtocolVersion($version));
+        $this->assertEquals($version, $this->message->getProtocolVersion());
+
+        $version = '0.9';
+        $this->setExpectedException('Exception');
+        $this->message->withProtocolVersion($version);
     }
 
 }
+
--- t/Fake/Message.php
+++ t/Fake/Message.php
@@ -3,11 +3,92 @@
 namespace ESuite\Fake;
 
 use \Psr\Http\Message\MessageInterface;
+use \Psr\Http\Message\StreamInterface;
 
 
-class Message extends MessageInterface
+class Message implements MessageInterface
 {
+    private $protocolVersion;
 
-	
+
+    public function __construct()
+    {
+        $this->protocolVersion = '1.1';
+    }
+
+    private function protocolVersionValid($version)
+    {
+        if (!in_array($version, array('1.0', '1.1'))) {
+            throw new \Exception('Not valid protocol version.');
+        }
+    }
+
+
+    // interface methods
+
+    public function getProtocolVersion()
+    {
+        return $this->protocolVersion;
+    }
+
+    
+    public function withProtocolVersion($version)
+    {
+        $this->protocolVersionValid($version);
+        $this->protocolVersion = $version;
+        return $this;
+    }
+
+    
+    public function getHeaders()
+    {
+
+    }
+
+   
+    public function hasHeader($name)
+    {
+
+    }
+
+    public function getHeader($name)
+    {
+
+    }
+
+
+    public function getHeaderLine($name)
+    {
+
+    }
+
+
+    public function withHeader($name, $value)
+    {
+
+    }
+
+   
+    public function withAddedHeader($name, $value)
+    {
+
+    }
+
+   
+    public function withoutHeader($name)
+    {
+
+    }
+
+   
+    public function getBody()
+    {
+
+    }
+
+    public function withBody(StreamInterface $body)
+    {
+
+    }
 
 }
