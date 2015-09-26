66e61aa87ed2c1beae148e2b1c9be33868c3492a
Mike Whiting <mail@mikejw.co.uk>
2015-09-26 11:09:42 +0100

    stream size with multibyte.

--- t/Experimental/StreamTest.php
+++ t/Experimental/StreamTest.php
@@ -13,6 +13,8 @@ class StreamTest extends ESuiteTest
     protected function setUp()
     {
         $this->stream = new \ESuite\Fake\Stream();
+        $this->stream->write('foo');
+        $this->stream->rewind();
     }
 
     public function testToString()
@@ -28,7 +30,23 @@ class StreamTest extends ESuiteTest
         $this->assertTrue($this->stream->isClosed());
     }    
 
+    public function testDetatch()
+    {
+        $d = $this->stream->detach();    
+        $this->assertTrue($this->stream->isClosed());
+        $this->assertEquals('foo', stream_get_contents($d));        
+    }
 
+
+    public function testGetSize()
+    {
+        $this->assertEquals(3, $this->stream->getSize());
+
+        $this->stream->reset();
+        $this->stream->write("¡\u0800");
+        echo $this->stream->getSize();
+
+    }
 
 
 }
--- t/Fake/Stream.php
+++ t/Fake/Stream.php
@@ -10,9 +10,20 @@ class Stream implements StreamInterface
 
     public function __construct()
     {
+        $this->init();
+    }
+
+
+    private function init()
+    {
         $this->data = fopen('php://memory', 'r+');
-        fwrite($this->data, 'foo');
-        rewind($this->data);
+    }
+
+
+    public function reset()
+    {
+        $this->close();
+        $this->init();
     }
 
 
@@ -23,6 +34,68 @@ class Stream implements StreamInterface
     }
 
 
+    /** 
+     * (From php.net)
+     * Count the number of bytes of a given string.
+     * Input string is expected to be ASCII or UTF-8 encoded.
+     * Warning: the function doesn't return the number of chars
+     * in the string, but the number of bytes.
+     * See http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
+     * for information on UTF-8.
+     *
+     * @param string $str The string to compute number of bytes
+     *
+     * @return The length in bytes of the given string.
+     */
+    private function strBytes($str)
+    {
+        // STRINGS ARE EXPECTED TO BE IN ASCII OR UTF-8 FORMAT
+        
+        // Number of characters in string
+        $strlen_var = strlen($str);
+        
+        // string bytes counter
+        $d = 0;
+        
+        /*
+         * Iterate over every character in the string,
+         * escaping with a slash or encoding to UTF-8 where necessary
+         */
+        for ($c = 0; $c < $strlen_var; ++$c) {
+            $ord_var_c = ord($str{$c});
+            switch(true) {
+            case(($ord_var_c >= 0x20) && ($ord_var_c <= 0x7F)):
+                // characters U-00000000 - U-0000007F (same as ASCII)
+                $d++;
+                break;
+            case(($ord_var_c & 0xE0) == 0xC0):
+                // characters U-00000080 - U-000007FF, mask 110XXXXX
+                $d+=2;
+                break;
+            case(($ord_var_c & 0xF0) == 0xE0):
+                // characters U-00000800 - U-0000FFFF, mask 1110XXXX
+                $d+=3;
+                break;
+            case(($ord_var_c & 0xF8) == 0xF0):
+                // characters U-00010000 - U-001FFFFF, mask 11110XXX
+                $d+=4;
+                break;
+            case(($ord_var_c & 0xFC) == 0xF8):
+                // characters U-00200000 - U-03FFFFFF, mask 111110XX
+                $d+=5;
+                break;
+            case(($ord_var_c & 0xFE) == 0xFC):
+                // characters U-04000000 - U-7FFFFFFF, mask 1111110X
+                $d+=6;
+                break;
+            default:
+                $d++;
+            };
+        };
+        return $d;
+    }
+
+
 
 
     // interface methods
@@ -30,7 +103,7 @@ class Stream implements StreamInterface
 
     public function __toString()
     {
-        return stream_get_contents($this->data);
+        return $this->getContents();
     }
 
     public function close()
@@ -40,12 +113,21 @@ class Stream implements StreamInterface
 
     public function detach()
     {
-
+        if (!$this->isClosed($this->data)) {
+            $new = fopen('php://memory', 'r+');
+            stream_copy_to_stream($this->data, $new);
+            $this->close();
+            rewind($new);
+            return $new;
+        } else {
+            return NULL;
+        }
     }
 
     public function getSize()
     {
-
+        $contents = $this->__toString();
+        return $this->strBytes($contents);
     }
 
     public function tell()
@@ -70,8 +152,9 @@ class Stream implements StreamInterface
 
     public function rewind()
     {
-
+        rewind($this->data);
     }
+
 
     public function isWritable()
     {
@@ -80,8 +163,9 @@ class Stream implements StreamInterface
 
     public function write($string)
     {
-
+        fwrite($this->data, $string);   
     }
+
 
     public function isReadable()
     {
@@ -95,7 +179,8 @@ class Stream implements StreamInterface
 
     public function getContents()
     {
-
+        $this->rewind();
+        return stream_get_contents($this->data);
     }
 
     public function getMetadata($key = null)
