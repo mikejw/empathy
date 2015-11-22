5d88d76bb77cc84330e32c5df68e21784755dc01
Mike Whiting <mail@mikejw.co.uk>
2015-11-22 12:38:53 +0000

    removing redbean stuff.

--- t/run_tests.sh
+++ t/run_tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #../vendor/bin/phpunit --testsuite="mvc"
-#../vendor/bin/phpunit ./MVC/EntityTest.php
-../vendor/bin/phpunit ./MVC/RedbeanTest.php
+../vendor/bin/phpunit ./MVC/EntityTest.php
 #../vendor/bin/phpunit .
 
 
