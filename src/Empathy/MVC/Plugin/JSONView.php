264b0ed00e419441591e3945fb6d025c47696b36
Mike Whiting <mikejw3@gmail.com>
2020-01-11 19:29:48 +0000

    styling default empathy pages
    styling default empathy pages
    
    "upstream" changes (to victor2)
    
    common.js fix
    
    change default fetch mode
    
    small fix
    
    fix uri tests
    
    phpunit version fixes
    
    style fixes
    
    style fixes
    
    adding sast analysis
    
    fix conditional centering (and removal of some legacy code)
    
    slight presenation tweak
    
    centering
    
    small fix
    
    add redirect
    
    redirect conditinally
    
    fix for JSONView
    
    expose error object return code in response (now standardised)
    
    fixing tests (docker)
    
    cleanup
    
    tidying
    
    small improvements
    
    copy blog images js and upgrade tinymce
    
    extract icons
    
    small fix?
    
    small fix?
    
    undo fixes
    
    add to readme
    
    bump version

--- src/Empathy/MVC/Plugin/JSONView.php
+++ src/Empathy/MVC/Plugin/JSONView.php
@@ -3,6 +3,7 @@
 namespace Empathy\MVC\Plugin;
 
 use Empathy\MVC\Plugin as Plugin;
+use Empathy\MVC\Testable;
 
 /**
  * Empathy JSONView Plugin
@@ -45,15 +46,22 @@ class JSONView extends Plugin implements PreEvent, Presentation
         // purpose because of cases where debug information is sought but (slower) formatting
         // is not required.
         $force_formatted = (defined('ELIB_FORCE_FORMATTED') && ELIB_FORCE_FORMATTED);
-
-        if (!(defined('MVC_TEST_MODE') && MVC_TEST_MODE)) {
-            header('Content-type: application/json');
-        }
+        
+        Testable::header('Content-type: application/json');
 
         if (is_object($this->output) &&
            (get_class($this->output) ==  $this->return_ob ||
             get_class($this->output) == $this->error_ob)) {
             $output = (string) $this->output;
+
+            if (get_class($this->output) == $this->error_ob) {
+                $header = 'HTTP/1.1 '
+                    .$this->output->getCode()
+                    .' '
+                    .$this->return_codes::getName($this->output->getCode())
+                ;
+                Testable::header($header);
+            }
 
             if (false !== ($callback = $this->output->getJSONPCallback())) {
                 $output = $callback.'('.$output.')';
@@ -94,10 +102,17 @@ class JSONView extends Plugin implements PreEvent, Presentation
         $e_ob = $this->error_ob;
 
         if (!$debug) {
-            $r = new $e_ob($rc::SERVER_ERROR, 'Server error.');
+            $r = new $e_ob($rc::Internal_Server_Error, 'Server error.');
         } else {
-            $r = new $e_ob(999, 'Exception: ' .$exception->getMessage(), 'SERVER_ERROR_EXPLICIT');
+            $r = new $e_ob($rc::Internal_Server_Error, 'Exception: ' .$exception->getMessage(), 'SERVER_ERROR_EXPLICIT');
         }
+
+        $header = 'HTTP/1.1 '
+            .$rc::Internal_Server_Error
+            .' '
+            .$this->return_codes::getName($rc::Internal_Server_Error);
+        Testable::header($header);
+
         $this->assign('default', $r, true);
         $this->display('');
     }
--- src/Empathy/MVC/Plugin/SmartySSL.php
+++ src/Empathy/MVC/Plugin/SmartySSL.php
@@ -26,11 +26,14 @@ class SmartySSL extends Smarty
             $this->switchInternal();
         }
 
+        $this->assignEmpathyDir();
+
         if (
             isset($this->config['force']) && $this->config['force'] ||
             \Empathy\MVC\Util\Misc::isSecure()
         ) {
-            echo str_replace('http://'.Config::get('WEB_ROOT'),
+            echo str_replace(
+                'http://'.Config::get('WEB_ROOT'),
                 'https://'.Config::get('WEB_ROOT'),
                 $this->smarty->fetch($template)
             );
