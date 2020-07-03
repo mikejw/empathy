2a9d10fe6b1b86f026110922e1d429b9fed3a09b
Mike Whiting <mikejw3@gmail.com>
2020-07-03 17:14:39 +0100

    add redirect

--- src/Empathy/MVC/Plugin/EDefault.php
+++ src/Empathy/MVC/Plugin/EDefault.php
@@ -4,6 +4,7 @@ namespace Empathy\MVC\Plugin;
 
 use Empathy\MVC\Testable;
 use Empathy\MVC\Plugin as Plugin;
+use Empathy\MVC\Config;
 
 /**
  * Empathy EDefault Plugin
@@ -24,5 +25,15 @@ class EDefault extends Plugin implements PreDispatch
         date_default_timezone_set('Europe/London');
         Testable::header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
         Testable::header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        $this->wwwRedirect();
+    }
+
+    private function wwwRedirect() {
+        $host = Config::get('WEB_ROOT');
+        $reqHost = $_SERVER['HTTP_HOST'];
+        if (0 === strpos($host, 'www') && false === strPos($reqHost,'www')) {
+            header("Location: //". $host . $_SERVER['REQUEST_URI']);
+            exit();
+        }
     }
 }
