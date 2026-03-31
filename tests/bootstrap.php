d5c500c24f3c24044607d693a92a7574c7e73dd3
Mike Whiting <mail@mikejw.co.uk>
2026-03-31 17:54:55 +0100

    pint fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,18 @@
 <?php
 
 declare(strict_types=1);
+use Empathy\MVC\Util\Testing\Util\Config;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-Empathy\MVC\Util\Testing\Util\Config::init(realpath(dirname(__FILE__)));
-if (Empathy\MVC\Util\Testing\Util\Config::get('set_test_mode')) {
-    define('MVC_TEST_MODE', Empathy\MVC\Util\Testing\Util\Config::get('set_test_mode'));
+$base = realpath(__DIR__);
+if ($base === false) {
+    throw new \RuntimeException('Could not resolve tests bootstrap path.');
 }
-if (Empathy\MVC\Util\Testing\Util\Config::get('set_test_mode_output')) {
-    define('MVC_TEST_OUTPUT_ON', Empathy\MVC\Util\Testing\Util\Config::get('set_test_mode_output'));
+Config::init($base);
+if (Config::get('set_test_mode')) {
+    define('MVC_TEST_MODE', Config::get('set_test_mode'));
+}
+if (Config::get('set_test_mode_output')) {
+    define('MVC_TEST_OUTPUT_ON', Config::get('set_test_mode_output'));
 }
